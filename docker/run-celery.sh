0fe073a8c4ab5234eaf4abe2668b235ce553756c
yegerless <Sergey.sys@mail.ru>
2025-03-30 20:23:26 +0300

    Add celery scheduled background task delete_expired_links

---
+++ docker/run-celery.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sleep 6
+
+cd src
+
+if [[ "${1}" == "celery" ]]; then
+#   celery --app=tasks.tasks:celery worker -l INFO
+    celery -A links.tasks:celery worker -B --loglevel=INFO
+elif [[ "${1}" == "flower" ]]; then
+  celery --app=tasks.tasks:celery flower
+ fi
