cd8ec2b6d8509a65e2bf1b98fa4af65a20bd28e4
yegerless <Sergey.sys@mail.ru>
2025-03-30 20:29:37 +0300

    Add flower

--- docker/run-celery.sh
+++ docker/run-celery.sh
@@ -8,5 +8,5 @@ if [[ "${1}" == "celery" ]]; then
 #   celery --app=tasks.tasks:celery worker -l INFO
     celery -A links.tasks:celery worker -B --loglevel=INFO
 elif [[ "${1}" == "flower" ]]; then
-  celery --app=tasks.tasks:celery flower
+  celery --app=links.tasks:celery flower
  fi
